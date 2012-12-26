69237e219aea11a66eaf2cf79b0501eba3d3c634
John McCutchan <john@johnmccutchan.com>
2012-12-25 16:46:22 -0800

    Allow clips to be constructed externally.
    Signed-off-by: John McCutchan <john@johnmccutchan.com>

--- lib/simple_audio/audio_clip.dart
+++ lib/simple_audio/audio_clip.dart
@@ -32,8 +32,12 @@ class AudioClip {
   bool _hasError = false;
   String _errorString = '';
   bool _isReadyToPlay = false;
+  bool _urlAbsolute = false;
 
   AudioClip._internal(this._manager, this._name, this._url);
+  AudioClip.external(this._manager, this._name, this._url) {
+    _urlAbsolute = true;
+  }
 
   void _empty() {
     _isReadyToPlay = false;
@@ -111,7 +115,11 @@ class AudioClip {
     request.on.load.add((e) => _onRequestSuccess(request, completer));
     request.on.error.add((e) => _onRequestError(request, completer));
     request.on.abort.add((e) => _onRequestError(request, completer));
-    request.open('GET', '${_manager.baseURL}/$url');
+    if (_urlAbsolute) {
+      request.open('GET', url);
+    } else {
+      request.open('GET', '${_manager.baseURL}/$url');
+    }
     request.send();
     return completer.future;
   }
@@ -125,7 +133,7 @@ class AudioClip {
     _buffer = _manager._context.createBuffer(numberOfChannels,
                                              numberOfChannels,
                                              sampleRate);
-    isReadyToPlay = true;
+    _isReadyToPlay = true;
   }
 
   /** Return the sample frames array for [channel]. Assuming a stereo setup,
